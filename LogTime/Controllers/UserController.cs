8451ab9aaef4410c1c70ba826446df9f93d20ca1
monterddq <thaidq@d3plus.com>
2017-11-24 10:01:13 +0700

    UserController

---
+++ LogTime/Controllers/UserController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using LogTime.DTO;
+using LogTime.Services;
+using LogTime.Models;
+using LogTime.Config;
+using LogTime.CustomException;
+using Microsoft.AspNetCore.Authorization;
+
+namespace LogTime.Controllers
+{
+    [ResponseCache(CacheProfileName = "LogTime")]
+    [Route("api/user")]
+    public class UserController : Controller
+    {
+        private UserService userService;
+        public UserController() => userService = new UserService();
+        [Route("login")]
+        [HttpPost]
+        public JsonResult Login(UserDTO obj)
+        {
+            var user = userService.FetchByUserName(obj.username);
+            if (user.password.Equals(Utility.Utility.Sha512Hash(obj.password)))
+                return Json(JwtManager.GenerateToken(user.user_id, user.username, user.fullname, user.email));
+            else
+                return Json(new ExceptionResult("401", "account or password is incorrect"));
+        }
+
+        [Route("details")]
+        [HttpGet]
+        [Authorize]
+        public JsonResult GetCurrentUser()
+        {
+            return Json("asdf");
+        }
+        //=> Json(userService.FetchByKey(User.Claims.FirstOrDefault().Value));
+
+    }
+}
---
+++ LogTime/CustomException/ExceptionResult.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LogTime.CustomException
+{
+    public class ExceptionResult
+    {
+        
+        public ExceptionResult(string code, string message)
+        {
+            this.code = code;
+            this.message = message;
+        }
+
+        public ExceptionResult() { }
+
+        private string code;
+        private string message;
+    }
+}
